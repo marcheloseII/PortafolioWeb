c0ee0681fac6e84b251573d0071449f490694af6
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-08-15 22:09:57 -0600

    ya funciona portafolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 // src/App.js
-
 import React from 'react';
-import ProjectsPage from './pages/ProjectsPage';  // Asegúrate de que esta importación es correcta
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import ContactPage from './pages/ContactPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <ProjectsPage />
-      {/* Otros componentes */}
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/App.css'; // Importa el archivo de estilos globales
+import './styles/App.css'; // Importa los estilos globales
 
-// Renderiza el componente App dentro del elemento con id 'root' en el HTML
 ReactDOM.render(
   <React.StrictMode>
     <App />
