1b0a8aa8cad6c5ca3dc3caa8f0a004ce75dcdf44
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-08-15 21:55:22 -0600

    Cambios implementados

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,12 +1,13 @@
 // src/pages/ProjectsPage.js
 import React from 'react';
 import ProjectCard from '../components/ProjectCard';
+import ProjectsPage from './ProjectsPage';
+
 
 const ProjectsPage = () => {
   // Ejemplo de proyectos
   const projects = [
     { id: 1, title: 'Proyecto 1', description: 'Descripción del Proyecto 1', image: '/path/to/image1.jpg' },
-    { id: 2, title: 'Proyecto 2', description: 'Descripción del Proyecto 2', image: '/path/to/image2.jpg' },
     // Agrega más proyectos aquí
   ];
 
